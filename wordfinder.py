7bb33ad2f37761eee22813a90b3776083f4ab750
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-08-11 11:28:03 -0500

    added word finder logic and tested it

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,19 @@
 """Word Finder: finds random words from a dictionary."""
-
-
+from random import choice
 class WordFinder:
-    ...
+    """Reads a list of words and returns a random word
+
+    >>> wf = WordFinder("/Users/student/words.txt")
+    3 words read
+    """
+    def __init__(self, folder):
+        self.folder = folder
+        self.file = open(self.folder,"r")
+        self.words = []
+        for line in self.file:
+            self.words.append(line)
+
+    def random(self):
+        random_word = choice(self.words)
+        end = len(random_word)-1
+        return random_word[:end]
